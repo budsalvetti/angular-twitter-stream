a584fbfa65bc3884f338f65f664f9e160b6bf1b6
budsalvetti <budsalvetti@gmail.com>
2022-12-21 13:59:37 -0800

    1 file modified

--- src/app/services/web-socket/web-socket.service.ts
+++ src/app/services/web-socket/web-socket.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { webSocket } from 'rxjs/webSocket'
 
 @Injectable()
 export class WebSocketService {
 
-  constructor() { }
+  twitterStreamUrl = 'https://api.twitter.com/2/tweets/sample/stream?tweet.fields=created_at,entities&user.fields=created_at,username,location';
+
+  constructor() {
+    const subject = webSocket('ws://localhost:8081');
+
+    subject.subscribe({
+      next: msg => console.log('message received: ' + msg), // Called whenever there is a message from the server.
+      error: err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+      complete: () => console.log('complete') // Called when connection is closed (for whatever reason).
+    });
+
+   }
 
 }
