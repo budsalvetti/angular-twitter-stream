e1df7340b76d868392e83ae8daf7fdb733ce3fb6
budsalvetti <budsalvetti@gmail.com>
2022-12-21 14:03:20 -0800

    1 file modified

--- src/app/services/web-socket/web-socket.service.ts
+++ src/app/services/web-socket/web-socket.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
-import { webSocket } from 'rxjs/webSocket'
+import { webSocket } from 'rxjs/webSocket';
 
 @Injectable()
 export class WebSocketService {
+  private readonly authToken =
+    'Bearer AAAAAAAAAAAAAAAAAAAAALGIkgEAAAAAuAf5xzhlwnIdGbEzUosVrRSY5os%3DRLMWue63Vjpz6Ngiw2mJmrogzCzySRyescdvQrQfQjHOXL4fYu';
 
-  twitterStreamUrl = 'https://api.twitter.com/2/tweets/sample/stream?tweet.fields=created_at,entities&user.fields=created_at,username,location';
+  twitterStreamUrl =
+    'https://api.twitter.com/2/tweets/sample/stream?tweet.fields=created_at,entities&user.fields=created_at,username,location';
 
   constructor() {
-    const subject = webSocket('ws://localhost:8081');
+    const subject = webSocket(
+      this.twitterStreamUrl + '?Authorization=${this.authToken'
+    );
 
     subject.subscribe({
-      next: msg => console.log('message received: ' + msg), // Called whenever there is a message from the server.
-      error: err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
-      complete: () => console.log('complete') // Called when connection is closed (for whatever reason).
+      next: (msg) => console.log('message received: ' + msg), // Called whenever there is a message from the server.
+      error: (err) => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+      complete: () => console.log('complete'), // Called when connection is closed (for whatever reason).
     });
-
-   }
-
+  }
 }
