c4074bdb390617bdefbacefe1f14c82f048b225b
budsalvetti <budsalvetti@gmail.com>
2022-12-21 21:40:32 -0800

    3 files modified, 1 added

--- src/app/services/web-socket/web-socket.service.ts
+++ src/app/services/web-socket/web-socket.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { webSocket } from 'rxjs/webSocket';
 
 @Injectable()
 export class WebSocketService {
@@ -9,15 +8,68 @@ export class WebSocketService {
   twitterStreamUrl =
     'https://api.twitter.com/2/tweets/sample/stream?tweet.fields=created_at,entities&user.fields=created_at,username,location';
 
-  constructor() {
-    const subject = webSocket(
-      this.twitterStreamUrl + '?Authorization=${this.authToken'
-    );
+  constructor() 
 
-    subject.subscribe({
-      next: (msg) => console.log('message received: ' + msg), // Called whenever there is a message from the server.
-      error: (err) => console.log(err), // Called if at any point WebSocket API signals some kind of error.
-      complete: () => console.log('complete'), // Called when connection is closed (for whatever reason).
-    });
-  }
+}
+
+
+getConnection(){
+
+// Open a live stream of roughly 1% random sample of publicly available Tweets
+// https://developer.twitter.com/en/docs/twitter-api/tweets/volume-streams/quick-start
+
+const needle = require('needle');
+
+// The code below sets the bearer token from your environment variables
+// To set environment variables on macOS or Linux, run the export command below from the terminal:
+// export BEARER_TOKEN='YOUR-TOKEN'
+const token = process.env.BEARER_TOKEN;
+
+const streamURL = 'https://api.twitter.com/2/tweets/sample/stream';
+
+function streamConnect(retryAttempt) {
+
+  const stream = needle.get(streamURL, {
+    headers: {
+      "User-Agent": "v2SampleStreamJS",
+      "Authorization": `Bearer ${token}`
+    },
+    timeout: 20000
+  });
+
+  stream.on('data', data => {
+    try {
+      const json = JSON.parse(data);
+      console.log(json);
+      // A successful connection resets retry count.
+      retryAttempt = 0;
+    } catch (e) {
+      // Catches error in case of 401 unauthorized error status.
+      if (data.status === 401) {
+        console.log(data);
+        process.exit(1);
+      } else if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
+        console.log(data.detail)
+        process.exit(1)
+      } else {
+        // Keep alive signal received. Do nothing.
+      }
+    }
+  }).on('err', error => {
+    if (error.code !== 'ECONNRESET') {
+      console.log(error.code);
+      process.exit(1);
+    } else {
+      // This reconnection logic will attempt to reconnect when a disconnection is detected.
+      // To avoid rate limits, this logic implements exponential backoff, so the wait time
+      // will increase if the client cannot reconnect to the stream.
+      setTimeout(() => {
+        console.warn("A connection error occurred. Reconnecting...")
+        streamConnect(++retryAttempt);
+      }, 2 ** retryAttempt);
+    }
+  });
+  return stream;
+}
+return streamConnect(0);
 }
