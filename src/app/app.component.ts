b8cf15ed330efca78763e6581f30d939c4591286
Bud Salvetti <budsalvetti@gmail.com>
2023-01-01 21:41:16 -0800

    final finshing touch of a nice spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,24 +31,27 @@ export class AppComponent implements OnInit {
     showYAxisLabel = true;
     yAxisLabel = 'Number Of Tweets with hash tags';
 
-    colorScheme = {
-        domain: ['#7c95fc', '#042cbc', '#444187', '#021f96']
-    };
+    colorScheme = {domain: ['#7c95fc', '#042cbc', '#444187', '#021f96']};
+
 
     streamIsOn = true;
 
+    waitingForData = true;
+
     cachedResponse: TwitterDataModel;
 
-    onSelect(event) {
-        console.log(event);
-    }
 
     ngOnInit(){
         // when the stream is on we will consume from the regular observable else from the the last emitted value
         // of the source observable this will allow us to freeze the chart to current
         this.twitterData$ = this.storeService.pipe(select(twitterDataSelector),
-                                                    map( data =>  {  // if the stream is turned on we will emit latest, if it is turned off then we will emit a cached value
-                                                                            console.log(JSON.stringify(data.twitterData));
+                                                    map( data =>  {
+                                                                            //turn off the spinner if we've gotten any real data
+                                                                            if(data.twitterData){
+                                                                                this.waitingForData = false;
+                                                                            }
+                                                                            // if the stream is turned on we will emit latest,
+                                                                             // if it is turned off then we will emit a cached value
                                                                             if(this.streamIsOn){
                                                                                 this.cachedResponse = data;
                                                                                 return  data.twitterData
