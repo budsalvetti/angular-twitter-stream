f5cb0951462608ccfb32efa9150475b5e50c80a3
Bud Salvetti <budsalvetti@gmail.com>
2023-01-03 22:15:37 -0800

    cleanup of redundant value

--- src/app/store/app.reducers.ts
+++ src/app/store/app.reducers.ts
@@ -1,15 +1,13 @@
 import * as AppActions  from './app.actions'
 import {ActionReducerMap, createReducer, on} from '@ngrx/store';
+import {TwitterDataModel} from "../model/twitter-data.model";
+
 export const TwitterDataFeatureKey = 'twitterData';
 
 export interface AppState {}
 
-export interface TwitterDataState {
-    tweetData: object[]
-}
-
 //set initial state of twitter data
-export const initialTwitterDataState: TwitterDataState = {tweetData: undefined};
+export const initialTwitterDataState: TwitterDataModel = {tweetData: undefined};
 
 // when 'twitterDataLoaded' action is dispatched we will update the store with the new data
 export const twitterDataReducer = createReducer(initialTwitterDataState,
--- src/app/store/app.selectors.ts
+++ src/app/store/app.selectors.ts
@@ -1,7 +1,8 @@
 import {createFeatureSelector, createSelector} from "@ngrx/store";
-import {TwitterDataState,TwitterDataFeatureKey} from './app.reducers';
+import {TwitterDataFeatureKey} from './app.reducers';
+import {TwitterDataModel} from "../model/twitter-data.model";
 
-export const selectTwitterDataState = createFeatureSelector<TwitterDataState>(TwitterDataFeatureKey);
+export const selectTwitterDataState = createFeatureSelector<TwitterDataModel>(TwitterDataFeatureKey);
 
 // create a memoized function so that the Observable
 // map operation will not be executed unless there is a change to state['twitterData']
